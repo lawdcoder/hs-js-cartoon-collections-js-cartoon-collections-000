8a0f403013e7896cbce09012e317b2b761b818db
Lawrence Agboifoh <lawdcoder@gmail.com>
2019-11-15 19:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
-function dwarfRollCall(dwarves) {
+var dwarves=["Doc", "Dopey", "Bashful", "Grumpy"];
+
+function dwarfRollCall(dwarves) 
+{
+  for(var i=0; i<dwarves.length; i++)
+  {
+    
+    var dwarf= [];
+    dwarf[i] = "${i} . ${dwarves[i]}"
+  }
+  return dwarf
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+
+
+function summonCaptainPlanet(planeteerCall)
+{
+  var newplaneteefCall = [];
+  for(var i=0; i<planeteerCall.length; i++)
+  {
+    newplaneteefCall[i]=planeteerCall[i].toUpperCase()
+    
+    
+  
+    
+  }
+  return newplaneteefCall.join('!');
 }
 
-function longPlaneteerCalls(words) {
+
+function longPlaneteerCalls(shortwords) 
+{
+  for(var i=0; i<shortwords.length; i++)
+  {
+    var num = shortwords[i];
+  }
+  if(num.length > 4);
 }
 
-function findTheCheese (foods) {
+
+function findTheCheese (foods) 
+{
+  var cheesearray = ["camon bear", "Chatter", "Guoder"];
+  for(var i =0; i<foods.length; i++)
+  {
+    if(cheesearray.includes(food[i]))
+    {
+      return foods[i];
+    
+  }
+  
+  
+  }
+}
+
+function startwithB(bArray)
+{
+  var matchArray = [];
+  for(var i= 0; i<bArray.length; i++)
+  {
+    if( bArray[i].matchesB())
+    {
+      matchArray.push(bArray[i])
+    }
+    
+  }
+  
+  return matchArray
 }
